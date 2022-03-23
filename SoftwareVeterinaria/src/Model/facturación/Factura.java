1a973456028a19626a3bc3f6a4709b9c4b4567a2
DavidSisalima7 <97250207+DavidSisalima7@users.noreply.github.com>
2022-03-23 18:52:48 -0500

    Modelos utilizados en factura
    Creación de clases y modelos que se utilizaran en la facturación.
    -Factura
    -Detalle Producto
    -Detalle Servicios

---
+++ SoftwareVeterinaria/src/Model/facturación/Factura.java
@@ -0,0 +1,75 @@
+
+package Model.facturación;
+
+import java.sql.Date;
+
+
+
+/**
+ *
+ * @author Usuario
+ */
+public class Factura {
+    
+    private String codigo_factura;
+    private String codigo_medico;
+    private String codigo_cliente;
+    private Date  fecha;
+    private boolean habilitado;
+    
+    //Constructores
+
+    public Factura() {
+    }
+
+    public Factura(String codigo_factura, String codigo_medico, String codigo_cliente, Date fecha, boolean habilitado) {
+        this.codigo_factura = codigo_factura;
+        this.codigo_medico = codigo_medico;
+        this.codigo_cliente = codigo_cliente;
+        this.fecha = fecha;
+        this.habilitado = habilitado;
+    }
+    
+    //Getters and setters
+
+    public String getCodigo_factura() {
+        return codigo_factura;
+    }
+
+    public void setCodigo_factura(String codigo_factura) {
+        this.codigo_factura = codigo_factura;
+    }
+
+    public String getCodigo_medico() {
+        return codigo_medico;
+    }
+
+    public void setCodigo_medico(String codigo_medico) {
+        this.codigo_medico = codigo_medico;
+    }
+
+    public String getCodigo_cliente() {
+        return codigo_cliente;
+    }
+
+    public void setCodigo_cliente(String codigo_cliente) {
+        this.codigo_cliente = codigo_cliente;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public boolean isHabilitado() {
+        return habilitado;
+    }
+
+    public void setHabilitado(boolean habilitado) {
+        this.habilitado = habilitado;
+    }
+    
+}
